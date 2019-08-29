fa1ef9446ea20850844797fc5e6e3e883f04cacc
Konstantin <kostilios@yandex.ru>
2019-08-29 14:49:54 +0300

    chop info restyling

--- src/components/layouts/ControlPanel/components/ControlPanelInfo/ControlPanelInfo.js
+++ src/components/layouts/ControlPanel/components/ControlPanelInfo/ControlPanelInfo.js
@@ -1,40 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './ControlPanelInfo.scss';
 
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  ListGroup,
-  ListGroupItem,
-} from 'reactstrap';
+import ChopTable from '../ChopTable';
+import ChopInfo from '../ChopInfo';
 
-const ControlPanelInfo = ({ chops }) => (
-  <div className="chop-info-container d-flex py-1 flex-row justify-content-start align-items-stretch">
-    {chops.map((chop, index) => (
-      <Card key={index} className="chop-card my-0">
-        <CardHeader>
-          {chop.name}
-        </CardHeader>
-        <CardBody className="p-0">
-          <ListGroup flush className="m-0">
-            <ListGroupItem>
-              <strong>Тел.: </strong>
-              <a href={`tel:${chop.phone}`}>
-                {chop.phone}
-              </a>
-            </ListGroupItem>
-            <ListGroupItem>
-              <strong>Дополнительно: </strong>
-              {chop.notes}
-            </ListGroupItem>
-          </ListGroup>
-        </CardBody>
-      </Card>
-    ))}
-  </div>
-);
+const ControlPanelInfo = ({ chops }) => {
+  const [chopSelected, selectChop] = useState(chops[0]);
+
+  const onClick = (index) => {
+    selectChop(chops[index]);
+  };
+
+  return (
+    <div className="chops-container d-flex">
+      <ChopTable
+        chops={chops}
+        onClick={onClick}
+        activeChopIndex={chops.indexOf(chopSelected)}
+      />
+      <ChopInfo chop={chopSelected} />
+    </div>
+  );
+};
 
 ControlPanelInfo.propTypes = {
   chops: PropTypes.array.isRequired,
