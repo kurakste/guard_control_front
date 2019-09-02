09bbc9056d0e2e7d73f0181505aabd28ae4cfb6a
Konstantin <kostilios@yandex.ru>
2019-09-02 11:58:27 +0300

    added map

--- src/components/layouts/ControlPanel/components/ControlPanelMap/ControlPanelMap.js
+++ src/components/layouts/ControlPanel/components/ControlPanelMap/ControlPanelMap.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
 import './ControlPanelMap.scss';
 
 const ControlPanelMap = () => (
-  <div className="map-container flex-grow-1"></div>
+  <div className="map-container">
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: 'AIzaSyCMm5CY1pV5wtF1-P2ukFI9-kQVIs0ZOa0' }}
+      defaultCenter={{ lat: 59.95, lng: 30.33 }}
+      defaultZoom={11}
+    >
+    </GoogleMapReact>
+  </div>
 );
 
 export default ControlPanelMap;
